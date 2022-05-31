4c48fd922793726f3c8b08e86c2744f4e6ef4eff
Alim <56486795+Alimbo-zzz@users.noreply.github.com>
2022-05-31 16:48:19 +0300

    fix req login -

--- Server/Front-end/game-page.js
+++ Server/Front-end/game-page.js
@@ -182,3 +182,30 @@ try {
 		// document.getElementById('response').append(e.message);
 }
 }
+
+
+
+function sendRequest(method, url, body = null){
+  return new Promise((resolve, reject)=>{
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Content-type', 'application/json');
+
+    xhr.onload = () =>{
+      if(xhr.status >= 400){
+        reject(xhr.response);
+      }else{
+        resolve(xhr.response);
+      }
+    }
+
+    xhr.onerror = () =>{
+      reject(xhr.response);
+    }
+
+
+    xhr.send(JSON.stringify(body));
+  })
+}
